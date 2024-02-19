8225572fe614766b4d2c5303577844451542b1ba
Raman Sharma <kukreti.rs@gmail.com>
2024-02-19 20:57:57 +0530

    Added pagination in dashboard page.

--- src/components/TableComponent.jsx
+++ src/components/TableComponent.jsx
@@ -1,4 +1,13 @@
-import { Card, Typography, Chip, Tooltip } from "@material-tailwind/react";
+import {
+  Card,
+  Typography,
+  Chip,
+  Tooltip,
+  CardBody,
+  CardFooter,
+  IconButton,
+  Button,
+} from "@material-tailwind/react";
 
 const TABLE_HEAD = [
   "Title",
@@ -14,115 +23,116 @@ export default function TableWithStripedRows({ data }) {
   console.log(data);
   return (
     <Card className="h-full w-full overflow-scroll">
-      <table className="w-full min-w-max table-auto text-left">
-        <thead>
-          <tr>
-            {TABLE_HEAD.map((head) => (
-              <th
-                key={head}
-                className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
-              >
-                <Typography
-                  variant="small"
-                  color="blue-gray"
-                  className="font-normal leading-none opacity-70"
+      <CardBody>
+        <table className="w-full min-w-max table-auto text-left">
+          <thead>
+            <tr>
+              {TABLE_HEAD.map((head) => (
+                <th
+                  key={head}
+                  className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
                 >
-                  {head}
-                </Typography>
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map(
-            (
-              {
-                id,
-                taskType,
-                associateWith,
-                dateAdded,
-                dateModified,
-                description,
-                title,
-                timeTaken,
-              },
-              index
-            ) => (
-              <tr key={id} className="even:bg-blue-gray-50/50">
-                <td className="p-4">
                   <Typography
                     variant="small"
                     color="blue-gray"
-                    className="font-normal"
+                    className="font-normal leading-none opacity-70"
                   >
-                    {title}
+                    {head}
                   </Typography>
-                </td>
-                <td className="p-4">
-                  <article
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal text-wrap"
-                  >
-                    {description.length > 50 ? (
-                      <Tooltip content={description} placement="top">
-                        <p>{description.substring(0, 47) + "..."}</p>
-                      </Tooltip>
-                    ) : (
-                      description
-                    )}
-                  </article>
-                </td>
-                <td className="p-4">
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                  >
-                    {dateAdded}
-                  </Typography>
-                </td>
-                <td className="p-4">
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                  >
-                    {dateModified}
-                  </Typography>
-                </td>
-                <td className="p-4">
-                  {/* <Typography
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map(
+              (
+                {
+                  id,
+                  taskType,
+                  associateWith,
+                  dateAdded,
+                  dateModified,
+                  description,
+                  title,
+                  timeTaken,
+                },
+                index
+              ) => (
+                <tr key={id} className="even:bg-blue-gray-50/50">
+                  <td className="p-4">
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {title}
+                    </Typography>
+                  </td>
+                  <td className="p-4">
+                    <article
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal text-wrap"
+                    >
+                      {description.length > 50 ? (
+                        <Tooltip content={description} placement="top">
+                          <p>{description.substring(0, 47) + "..."}</p>
+                        </Tooltip>
+                      ) : (
+                        description
+                      )}
+                    </article>
+                  </td>
+                  <td className="p-4">
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {dateAdded}
+                    </Typography>
+                  </td>
+                  <td className="p-4">
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {dateModified}
+                    </Typography>
+                  </td>
+                  <td className="p-4">
+                    {/* <Typography
                     variant="small"
                     color="blue-gray"
                     className="font-normal"
                   >
                     {taskId}
                   </Typography> */}
-                  <Chip
-                    size="sm"
-                    variant="ghost"
-                    className="text-center"
-                    value={taskType}
-                    color={
-                      taskType === "feature"
-                        ? "green"
-                        : taskType === "bug"
-                        ? "pink"
-                        : "amber"
-                    }
-                  />
-                </td>
-                <td className="p-4">
-                  <Chip
-                    size="sm"
-                    variant="ghost"
-                    className="text-center"
-                    value={associateWith}
-                    color={associateWith === "internal" ? "cyan" : "indigo"}
-                  />
-                </td>
-                {/* <td className="p-4">
+                    <Chip
+                      size="sm"
+                      variant="ghost"
+                      className="text-center"
+                      value={taskType}
+                      color={
+                        taskType === "feature"
+                          ? "green"
+                          : taskType === "bug"
+                          ? "pink"
+                          : "amber"
+                      }
+                    />
+                  </td>
+                  <td className="p-4">
+                    <Chip
+                      size="sm"
+                      variant="ghost"
+                      className="text-center"
+                      value={associateWith}
+                      color={associateWith === "internal" ? "cyan" : "indigo"}
+                    />
+                  </td>
+                  {/* <td className="p-4">
                   <Typography
                     as="a"
                     href="#"
@@ -133,11 +143,43 @@ export default function TableWithStripedRows({ data }) {
                     Edit
                   </Typography>
                 </td> */}
-              </tr>
-            )
-          )}
-        </tbody>
-      </table>
+                </tr>
+              )
+            )}
+          </tbody>
+        </table>
+      </CardBody>
+      <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
+        <Button variant="outlined" size="sm">
+          Previous
+        </Button>
+        <div className="flex items-center gap-2">
+          <IconButton variant="outlined" size="sm">
+            1
+          </IconButton>
+          <IconButton variant="text" size="sm">
+            2
+          </IconButton>
+          <IconButton variant="text" size="sm">
+            3
+          </IconButton>
+          <IconButton variant="text" size="sm">
+            ...
+          </IconButton>
+          <IconButton variant="text" size="sm">
+            8
+          </IconButton>
+          <IconButton variant="text" size="sm">
+            9
+          </IconButton>
+          <IconButton variant="text" size="sm">
+            10
+          </IconButton>
+        </div>
+        <Button variant="outlined" size="sm">
+          Next
+        </Button>
+      </CardFooter>
     </Card>
   );
 }
